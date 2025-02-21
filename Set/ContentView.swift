98f35be70dc053c1f54b3ec45a748db6b4f89af1
zcelaloglu <zafer.celaloglu@gmail.com>
2025-02-21 11:09:02 +0300

    Initial Commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Zafer CELALOGLU on 21.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
